faf301b8a80b774ee4f7689041a6c4f8fc6ae9c0
gisma <gisma@users.noreply.github.com>
2023-06-26 08:24:08 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,16 @@
+moreaEventData = [
+  {title: "Spatial Prediction of Forest Microclimate", url: "/LV-19-d19-006/modules/microclimate-analysis-gis", start: "2022-01-28T23:00"},
+  {title: "Apply different models for microclimate analysis and write a shorte note", url: "/LV-19-d19-006/morea/microclimate-analysis/assessment-microclimate-analysis-gis-1.html", start: "2022-02-15T09:00"},
+  {title: "Use the course scripts repository", url: "/LV-19-d19-006/morea/microclimate-analysis/experience-microclimate-analysis-gis-1.html", start: "2022-01-28T23:00"},
+  {title: "Knowledge and prerequisites for the course", url: "/LV-19-d19-006/modules/module-prerequisites", start: "2022-11-01", end: "2022-11-08"},
+  {title: "Assessment getting organized", url: "/LV-19-d19-006/morea/prerequisites/assessment-pre-know-how-1.html", start: "2021-11-02T23:59"},
+  {title: "Write a short communication about your project", url: "/LV-19-d19-006/morea/scientific-reporting/experience-scientific-writing-1.html", start: "2022-02-14T23:00"},
+  {title: "Write a short note", url: "/LV-19-d19-006/morea/scientific-reporting/assessment-scientific-writing-1.html", start: "2022-02-15T09:00"},
+  {title: "Scientific Reporting", url: "/LV-19-d19-006/modules/scientific-reporting", start: "2022-02-15T23:00"},
+  {title: "Assessment R-spatial GIS", url: "/LV-19-d19-006/morea/geocomputation-with-r/assessment-spatial-r-gis-1.html", start: "2021-11-09T23:59"},
+  {title: "Introduction to Spatial R-GIS", url: "/LV-19-d19-006/modules/spatial-r-gis", start: "2021-11-02", end: "2021-11-16"},
+  {title: "Assessment Website for Course Documentation", url: "/LV-19-d19-006/morea/gis-reproducible-research/assessment-gis-reproducible-research.html", start: "2021-11-16T23:59"},
+  {title: "Reproducible Research", url: "/LV-19-d19-006/modules/gis-reproducible-research", start: "2022-11-01", end: "2022-11-08"},
+  {title: "Assessment: LiDAR based Forest Indices", url: "/LV-19-d19-006/morea/forest-lidar/assessment-forest-lidar-gis-1.html", start: "2021-11-30T23:59"},
+  {title: "Deriving ecological indices from LiDAR data", url: "/LV-19-d19-006/modules/forest-lidar-gis", start: "2021-11-16", end: "2021-11-29"}
+]
